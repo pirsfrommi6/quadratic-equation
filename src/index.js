f8e31f96fcdb4a44cbfb3ea69f5ff4d69a0de531
pirs <pirsfrommi6@gmail.com>
2018-09-28 13:20:19 +0300

    task4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+	const regexp = /(-?\d{1,})\s[*]\sx\^2\s([+-])\s(\d+)\s\*\sx\s([-+])\s(\d+)/;
+	const mass = equation.match(regexp);
+	const a = +mass[1];
+	const b = +(mass[2] + mass[3]);
+	const c = +(mass[4] + mass[5]);
+	const d = Math.pow(b, 2) - 4 * a * c;
+	return [(Math.round(-b - Math.sqrt(d))/(2 * a)), Math.round((-b + Math.sqrt(d)) / (2 * a))].sort((a, b) => {return a - b});
 }
